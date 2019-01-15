73d5501ba4506da9d10c14a26d634f5d15bd0676
Vincent Baylly <vincent.baylly@gmail.com>
2019-01-14 19:12:24 -0500

    Create the angular module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
-import { MatIconModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GoogleMapsComponent } from './google-maps/google-maps.component';
+import { GoogleMapsModule } from './google-maps/google-maps.module';
 import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GoogleMapsComponent,
     MessagesComponent
   ],
   imports: [
-    MatIconModule,
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    BrowserAnimationsModule
+    GoogleMapsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
